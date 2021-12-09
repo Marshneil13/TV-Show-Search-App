cb97a6a7f505e4be4f1937ca95cf484136feb7cf
marshnil_13 <btech10225.20@bitmesra.ac.in>
2021-12-09 15:56:16 +0530

    final coomit

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const form = document.querySelector('#searchForm');
+const showInput = document.querySelector('#showSearch');
+const refresh = document.querySelector('#refresh');
+const div = document.querySelector('.display')
 
 form.addEventListener('submit', async function(e){
     e.preventDefault();//to prevent the browser from executing the default action
-    const searchTerm = form.elements.query.value;
+    const img = document.querySelectorAll('img');
+    console.log(img);
+    const searchTerm = showInput.value;
+    console.log(searchTerm);
     const config = {params: {q: searchTerm}};//setting config values
+    console.log(config);
     const res= await axios.get(`https://api.tvmaze.com/search/shows`,config);
     displayShows(res.data);
     form.elements.query.value = "";
@@ -14,11 +21,18 @@ const displayShows = (shows) => {
         if(result.show.image){
         const img = document.createElement('img');
         img.src = result.show.image.medium;
-        document.body.append(img);
+        div.append(img);
         }
     }
 }
+const img = document.querySelectorAll('img');
+refresh.addEventListener('click', () => {
+    div.remove(img);
+})
 // app.js:12 Uncaught (in promise) TypeError: Cannot read properties of null (reading 'medium')
 //     at displayShows (app.js:12)
 //     at HTMLFormElement.<anonymous> (app.js:6)
 // purpose of line 14
+// showInput.addEventListener('change',function (){
+//     document.body.remove('img');
+// });
